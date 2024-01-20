26935f5f09732df686d358f9e7b2638a567f0cdd
Gawwwi <gabinwilson05@gmail.com>
2024-01-20 23:55:55 +0100

    v0.6

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ next_level = False
 number_potion_health = 3
 up_round = False
 
+print(inter_ligne)
+
 while True:
 
     potion_helth = random.randint(15, 20)
@@ -18,7 +20,9 @@ while True:
 
 
     while up_round == False:
+
         if user_choice == "1": #attack_player
+            
             health_ennemy = health_ennemy - attack_player
             print(f"Vous avez infligé {attack_player} points de dégats à l'ennemi")
             next_level = True
@@ -30,20 +34,31 @@ while True:
             number_potion_health = number_potion_health - 1
 
             if number_potion_health < 0:
+
                 print("Vous n'avez plus de potions")
                 next_level = False
                 print(inter_ligne)
                 up_round = False
                 break
 
-            if number_potion_health >= 0 :
+            elif number_potion_health >= 0 :
+
                 health_player = health_player + potion_helth
                 print(f"Vous récupérez {potion_helth} points de vie ({number_potion_health} restantes)")
                 next_level = True
                 up_round = True
                 break
+
+            else: #gestion erreur
+
+                print("Veuillez entrer un choix valide")
+                print(inter_ligne)
+                next_level = False
+                up_round = False
+                break
             
-        else: 
+        else: #gestion erreur
+
             print("Veuillez entrer un choix valide")
             print(inter_ligne)
             next_level = False
@@ -51,8 +66,19 @@ while True:
             break
 
     if next_level == True :
+
         health_player = health_player - attack_ennemy
         print(f"L'ennemi vous a infligé {attack_ennemy} points de dégats")
         print(f"Il vous reste {health_player} points de vie.")
         print(f"Il reste {health_ennemy} points de vie a l'ennemi.")
         print(inter_ligne)
+
+    if up_round == True:
+
+        health_player = health_player - attack_ennemy
+        print("Vous passez votre tour ...")
+        print(f"L'ennemi vous a infligé {attack_ennemy} points de dégats")
+        print(f"Il vous reste {health_player} points de vie.")
+        print(f"Il reste {health_ennemy} points de vie a l'ennemi.")
+        print(inter_ligne)
+        up_round = False
