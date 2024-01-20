eb1f539285302333192f164ba39555c5cc11f90e
Gawwwi <gabinwilson05@gmail.com>
2024-01-20 12:00:52 +0100

    v0.2

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Test")
+import random
+import sys 
+
+health_player = 50
+health_ennemy = 50
+attack_player = random.randint(5, 10)
+attack_ennemy = random.randint(5, 15)
+potion_helth = random.randint(15, 20)
