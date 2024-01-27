c255a62d6b1d7c989e421b12d1809ec9b88662e7
Gabin <gabinwilson05@gmail.com>
2024-01-27 12:45:46 +0100

    v1.2

--- main.py
+++ main.py
@@ -11,20 +11,51 @@ INTER_LIGNE = ("-" * 50)
 
 print(INTER_LIGNE)
 
-def relaunch(relaunch_s, relaunch_choice, INTER_LIGNE):
-    while relaunch_s == True:
 
-        if relaunch_choice == "1":
-            print("Vous venez de relancer la partie.")
-            print(INTER_LIGNE)
-            game(health_player, health_ennemy, number_potion_health, next_level, up_round, relaunch_s, INTER_LIGNE)
-        
-        elif relaunch_choice == "2":
-            print("Vous quitter le jeu.")
-            sys.exit()
+def regle (INTER_LIGNE):
+    print("""Voici les règles du jeu : 
+-Le jeu comporte deux joueurs : vous et un ennemi.
+-Vous commencez tous les deux avec 50 points de vie.
+-Votre personnage dispose de 3 potions qui vous permettent de récupérer des points de vie.
+-L'ennemi ne dispose d'aucune potion.
+-Chaque potion vous permet de récupérer un nombre aléatoire de points de vie, compris entre 15 et 50.
+-Votre attaque inflige à l'ennemi des dégâts aléatoires compris entre 5 et 10 points de vie.
+-L'attaque de l'ennemi vous inflige des dégâts aléatoires compris entre 5 et 15 points de vie.
+-Lorsque vous utilisez une potion, vous passez le prochain tour.
+          
+Note :
+
+À chaque tour, vous attaquez en premier. Il ne peut donc pas y avoir de match nul. Si lorsque vous attaquez, votre attaque fait descendre les points de vie de l'ennemi en dessous (ou égal à) 0, vous gagnez la partie sans que l'ennemi n'ait le temps de vous attaquer en retour.""")
+    print(INTER_LIGNE)
+
+
+def relaunch(relaunch_s, INTER_LIGNE):
+
+    while True:
+
+        relaunch_choice = input("Souhaitez vous rejouer ?\nEntrer 1 pour rejouer, ou 2 pour quitter le jeu : ")
+
+        while relaunch_s == True:
+
+            if relaunch_choice == "1":
+                print("Vous venez de relancer la partie.")
+                print(INTER_LIGNE)
+                game(health_player, health_ennemy, number_potion_health, next_level, up_round, relaunch_s, INTER_LIGNE)
+            
+            elif relaunch_choice == "2":
+                print("Vous quitter le jeu.")
+                print(INTER_LIGNE)
+                sys.exit()
+
+            else:
+                print("Veuillez entrer un choix valide.")
+                print(INTER_LIGNE)
+                break
 
 
 def game(health_player, health_ennemy, number_potion_health, next_level, up_round, relaunch_s, INTER_LIGNE):
+
+    regle(INTER_LIGNE)
 
     while True:
 
@@ -89,7 +120,7 @@ def game(health_player, health_ennemy, number_potion_health, next_level, up_roun
             print(f"Il reste {health_ennemy} points de vie a l'ennemi.")
             print(INTER_LIGNE)
 
-        if up_round == True: #Quand potion utilisé
+        if up_round == True and next_level == True: #Quand potion utilisé
 
             health_player = health_player - attack_ennemy
             print("Vous passez votre tour ...")
@@ -97,20 +128,25 @@ def game(health_player, health_ennemy, number_potion_health, next_level, up_roun
             print(f"Il vous reste {health_player} points de vie.")
             print(f"Il reste {health_ennemy} points de vie a l'ennemi.")
             print(INTER_LIGNE)
+
             up_round = False
 
         if health_ennemy <= 0: #WIN
             print("Vous avez gagné")
             print(INTER_LIGNE)
+
             relaunch_s = True
-            relaunch_choice = input("1, rejouer. 2, quitter")
-            relaunch(relaunch_s, relaunch_choice, INTER_LIGNE)
+            #relaunch_choice = input("1, rejouer. 2, quitter")
+            relaunch(relaunch_s, INTER_LIGNE)
         
         elif health_player <= 0: #LOOSE
             print("Tu as perdu")
             print(INTER_LIGNE)
-            relaunch_s = True
-            relaunch_choice = input("1, rejouer. 2, quitter")
-            relaunch(relaunch_s, relaunch_choice, INTER_LIGNE)
 
+            relaunch_s = True
+           # relaunch_choice = input("1, rejouer. 2, quitter")
+            relaunch(relaunch_s, INTER_LIGNE)
+
+
+regle(INTER_LIGNE)
 game(health_player, health_ennemy, number_potion_health, next_level, up_round, relaunch_s, INTER_LIGNE)
